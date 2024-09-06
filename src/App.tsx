629f7394d5a06fcd225656ff4419b3ae6b6ec50a
ndzy01 <46743100+ndzy01@users.noreply.github.com>
2024-09-06 16:01:11 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,132 +1,37 @@
-import React, { useState } from 'react';
-import { Drawer, FloatButton, Tree, TreeProps } from 'antd';
+import { MdCatalog, MdPreview } from 'md-editor-rt';
+import { v4 as uuidv4 } from 'uuid';
+import 'md-editor-rt/lib/style.css';
 import { useStore } from 'ndzy-utils';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Outlet,
-  useNavigate,
-} from 'react-router-dom';
-import AddArticle from './Add.tsx';
-import EditArticle from './Edit.tsx';
-import { View } from './View.tsx';
-import {
-  DeleteOutlined,
-  EditOutlined,
-  HomeOutlined,
-  MenuOutlined,
-  PlusOutlined,
-} from '@ant-design/icons';
+import { Drawer, FloatButton } from 'antd';
+import React, { useState } from 'react';
+import { MenuOutlined } from '@ant-design/icons';
 
-const ILayout: React.FC = () => {
-  const navigate = useNavigate();
+const scrollElement = document.documentElement;
+export const View = () => {
+  const id = 'id_md_' + uuidv4();
   const store = useStore();
-  const [expandedKeys, setExpandedKeys] = useState<React.Key[]>([]);
-  const [selectedKeys, setSelectedKeys] = useState<React.Key[]>([]);
-  const [autoExpandParent, setAutoExpandParent] = useState<boolean>(true);
   const [open, setOpen] = useState(false);
-
   const showDrawer = () => {
     setOpen(true);
   };
-
   const onClose = () => {
     setOpen(false);
   };
 
-  const onExpand: TreeProps['onExpand'] = (expandedKeysValue) => {
-    setExpandedKeys(expandedKeysValue);
-    setAutoExpandParent(false);
-  };
-
-  const onSelect: TreeProps['onSelect'] = (selectedKeysValue) => {
-    setSelectedKeys(selectedKeysValue);
-    if (selectedKeysValue.length === 0) return;
-    store.api.article.find(selectedKeysValue[0] as string).then();
-  };
-
   return (
-    <>
-      <Outlet />
-      <FloatButton.Group shape="circle" style={{ insetInlineEnd: 16 }}>
-        <FloatButton
-          icon={<HomeOutlined />}
-          onClick={() => {
-            navigate('/');
-          }}
-        />
-        <FloatButton icon={<MenuOutlined />} onClick={showDrawer} />
-        <FloatButton
-          icon={<PlusOutlined />}
-          onClick={() => {
-            navigate('/add');
-          }}
-        />
-        {store.article?.id && (
-          <>
-            <FloatButton
-              icon={<EditOutlined />}
-              onClick={() => {
-                navigate('/edit');
-              }}
-            />
-            <FloatButton
-              icon={<DeleteOutlined />}
-              onClick={() => {
-                store.api.article.del(store.article?.id as string).then(() => {
-                  window.location.reload();
-                });
-              }}
-            />
-          </>
-        )}
-        <FloatButton.BackTop visibilityHeight={0} />
-      </FloatButton.Group>
-      <Drawer title="目录" onClose={onClose} open={open}>
-        <Tree
-          treeData={store.articles as any}
-          height={600}
-          onExpand={onExpand}
-          expandedKeys={expandedKeys}
-          autoExpandParent={autoExpandParent}
-          onSelect={onSelect}
-          selectedKeys={selectedKeys}
-          titleRender={(item) => {
-            const title = item.title as React.ReactNode;
-            return <div>{title}</div>;
-          }}
-        />
-      </Drawer>
-    </>
+    store.article?.content && (
+      <>
+        <MdPreview editorId={id} modelValue={store.article?.content} />
+        <FloatButton.Group
+          shape="circle"
+          style={{ insetInlineEnd: 16, top: 16 }}
+        >
+          <FloatButton icon={<MenuOutlined />} onClick={showDrawer} />
+        </FloatButton.Group>
+        <Drawer title="目录" onClose={onClose} open={open}>
+          <MdCatalog editorId={id} scrollElement={scrollElement} />
+        </Drawer>
+      </>
+    )
   );
 };
-
-const router = createBrowserRouter([
-  {
-    id: 'root',
-    path: '/',
-    Component: ILayout,
-    children: [
-      {
-        path: '/',
-        Component: View,
-      },
-      {
-        path: '/add',
-        Component: AddArticle,
-      },
-      {
-        path: '/edit',
-        Component: EditArticle,
-      },
-    ],
-  },
-]);
-
-const App: React.FC = () => {
-  return (
-    <RouterProvider router={router} fallbackElement={<p>Initial Load...</p>} />
-  );
-};
-
-export default App;
